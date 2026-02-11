6663d8fa6c599fdbccd39575a2462060293283e3
T-Rizzle12 <92559393+T-Rizzle12@users.noreply.github.com>
2026-02-10 21:15:12 -0500

    Fix minor logic error in DefibrillatorScript

--- UsualScrap/Behaviors/DefibrillatorScript.cs
+++ UsualScrap/Behaviors/DefibrillatorScript.cs
@@ -404,12 +404,10 @@ namespace UsualScrap.Behaviors
                         PlayerScript.currentVoiceChatIngameSettings.InitializeComponents();
                     }
 
-                    if (PlayerScript.currentVoiceChatIngameSettings.voiceAudio == null)
+                    if (PlayerScript.currentVoiceChatIngameSettings.voiceAudio != null)
                     {
-                        return;
+                        PlayerScript.currentVoiceChatIngameSettings.voiceAudio.GetComponent<OccludeAudio>().overridingLowPass = false;
                     }
-
-                    PlayerScript.currentVoiceChatIngameSettings.voiceAudio.GetComponent<OccludeAudio>().overridingLowPass = false;
                 }
             }
             PlayerControllerB localplayercontroller = GameNetworkManager.Instance.localPlayerController;
@@ -447,4 +445,5 @@ namespace UsualScrap.Behaviors
         }
     }
 }
+
 
